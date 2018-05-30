d959e444bd92db24602c018d60843b771f02da65
Bernardo F <bern_freitas@hotmail.com>
2018-05-30 23:38:20 +0100

    login e regis

--- app/src/main/java/com/gp/gpproject/RegisterActivity.java
+++ app/src/main/java/com/gp/gpproject/RegisterActivity.java
@@ -1,11 +1,10 @@
 package com.gp.gpproject;
 
-<<<<<<< HEAD
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-=======
->>>>>>> Joao
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -64,8 +63,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         */
     }
 
-<<<<<<< HEAD
-=======
+
     /*
     public void sqlTest() {
         String email = textEmail.getText().toString().trim();
