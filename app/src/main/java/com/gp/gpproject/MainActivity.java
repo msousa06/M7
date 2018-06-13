7d6cfbcb4b86807bb0d6a908f5df6868b0d8ad4b
Bernardo F <bern_freitas@hotmail.com>
2018-06-13 20:08:00 +0100

    logout

--- app/src/main/java/com/gp/gpproject/MainActivity.java
+++ app/src/main/java/com/gp/gpproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnListaDocentes = (Button) findViewById(R.id.btnListaDocentes);
         btnListaDocentes.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(MainActivity.this,ListarDocentesActivity.class));
-            }
            @Override
            public void onClick(View v){
                startActivity(new Intent(MainActivity.this,ListarDocentesActivity.class));
@@ -57,6 +55,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this,AdicionarDisciplinaActivity.class));
+            }
+        });
+
+        Button logOut = (Button) findViewById(R.id.logOutBtn);
+        logOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
             }
         });
     }
