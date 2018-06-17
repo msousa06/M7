7f8a963a26d9619b1e4e1024a0233dc89163d509
Bernardo F <bern_freitas@hotmail.com>
2018-06-17 16:06:59 +0100

    email de notificação apos create vigilancia

--- app/src/main/java/com/gp/gpproject/AgendarVigilanciaActivity.java
+++ app/src/main/java/com/gp/gpproject/AgendarVigilanciaActivity.java
@@ -2,8 +2,10 @@ package com.gp.gpproject;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +19,6 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
-
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -63,10 +63,8 @@ public class AgendarVigilanciaActivity extends AppCompatActivity {
                     if (!mDisplayDate.getText().toString().equalsIgnoreCase("")) {
                         if (!mDisplayTime.getText().toString().equalsIgnoreCase("")) {
                             manager.insert_vigilancia(salatxt.getText().toString(), mDisplayDate.getText().toString(), mDisplayTime.getText().toString(), spinnerVig.getSelectedItem().toString(), spinnerUC.getSelectedItem().toString(), spinnerPontuacao.getSelectedItem().toString());
+                            sentNotification();
                             finish();
-                                manager.insert_vigilancia(salatxt.getText().toString(), mDisplayDate.getText().toString(), mDisplayTime.getText().toString(), spinnerVig.getSelectedItem().toString(), spinnerUC.getSelectedItem().toString(), spinnerPontuacao.getSelectedItem().toString());
-                                finish();
-
                         } else {
                             alertDialog = new AlertDialog.Builder(AgendarVigilanciaActivity.this).create();
                             alertDialog.setTitle("Erro");
@@ -181,5 +179,15 @@ public class AgendarVigilanciaActivity extends AppCompatActivity {
         }
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, items);
         spinnerPontuacao.setAdapter(adapter);
+    }
+
+    public void sentNotification(){
+        Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Notificação de Vigilancia");
+        intent.putExtra(Intent.EXTRA_TEXT, "Foi requisitado para uma vigilância no dia " + mDisplayDate.getText().toString() + "às " + mDisplayTime.getText().toString() + " horas." + "\n Disciplina: " + spinnerUC.getSelectedItem().toString());
+        intent.setData(Uri.parse("mailto:" + spinnerVig.getSelectedItem().toString())); // or just "mailto:" for blank
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+        startActivity(intent);
     }
 }
